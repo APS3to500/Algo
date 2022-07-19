05b29b5ac9d1b725fbdcab50f7bc6c44e0f498ff
jsw3788 <jsw3788@naver.com>
2022-07-20 00:58:09 +0900

    220720

---
+++ 백준/스터디/17136_색종이붙이기_정성우.py
@@ -0,0 +1,62 @@
+dist = [list(map(int, input().split())) for _ in range(10)]
+
+paper = [0, 5, 5, 5, 5, 5]
+
+ans = 987654321
+
+
+def cover(r, c, size, now):
+    # 색종이 붙이기
+    for a in range(r, r + size):
+        for b in range(c, c + size):
+            dist[a][b] = 0
+    paper[size] -= 1
+    dfs(now + 1)
+
+    # 색종이 떼기
+    for a in range(r, r + size):
+        for b in range(c, c + size):
+            dist[a][b] = 1
+    paper[size] += 1
+
+
+def dfs(now):
+    global ans
+    if now > ans:
+        return
+
+    for i in range(10):
+        for j in range(10):
+            if dist[i][j]:
+                for l in range(1, 6):
+                    flag = True
+                    if paper[l] > 0 and i + l <= 10 and j + l <= 10:
+                        for x in range(i, i + l):
+                            for y in range(j, j + l):
+                                if dist[x][y] == 0:
+                                    flag = False
+                                    break
+                            if not flag:
+                                break
+                        if flag:
+                            cover(i, j, l, now)
+                return
+
+    check = True
+    for i in range(10):
+        for j in range(10):
+            if dist[i][j]:
+                check = False
+                break
+        if not check:
+            break
+
+    if check:
+        ans = min(ans, now)
+
+
+dfs(0)
+if ans == 987654321:
+    print(-1)
+else:
+    print(ans)
