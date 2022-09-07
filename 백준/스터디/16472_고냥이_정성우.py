42042e57352b35c006a7fedfa5bdef81ccbba5db
jsw3788 <jsw3788@naver.com>
2022-09-07 21:26:21 +0900

    220907

---
+++ 백준/스터디/16472_고냥이_정성우.py
@@ -0,0 +1,53 @@
+n = int(input())
+s = input()
+
+ans = 1
+l = 0
+r = 0
+alphabet = {
+    "a": 0,
+    "b": 0,
+    "c": 0,
+    "d": 0,
+    "e": 0,
+    "f": 0,
+    "g": 0,
+    "h": 0,
+    "i": 0,
+    "j": 0,
+    "k": 0,
+    "l": 0,
+    "m": 0,
+    "n": 0,
+    "o": 0,
+    "p": 0,
+    "q": 0,
+    "r": 0,
+    "s": 0,
+    "t": 0,
+    "u": 0,
+    "v": 0,
+    "w": 0,
+    "x": 0,
+    "y": 0,
+    "z": 0
+}
+k = 0
+while r < len(s):
+
+    if not alphabet[s[r]]:
+        k += 1
+        if k > n:
+            while k > n:
+                alphabet[s[l]] -= 1
+                if not alphabet[s[l]]:
+                    k -= 1
+                l += 1
+
+    alphabet[s[r]] += 1
+    r += 1
+    cnt = r - l
+
+    ans = max(cnt, ans)
+
+print(ans)
